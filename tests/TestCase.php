55fadb5babcc49e5888c00ab0f712498fe4931d1
Carlos Sanchez <sanchezcl@gmail.com>
2021-11-30 21:55:56 -0300

    feat: run unit tests in memory database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,25 @@
 <?php
 
+use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use DatabaseMigrations {
+        runDatabaseMigrations as baseRunDatabaseMigrations;
+    }
+
+    /**
+     * Define hooks to migrate the database before and after each test.
+     *
+     * @return void
+     */
+    public function runDatabaseMigrations()
+    {
+        $this->baseRunDatabaseMigrations();
+        $this->artisan('db:seed');
+    }
+
     /**
      * Creates the application.
      *
